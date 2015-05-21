67e78b230dfd9410721bb2088257280971c663b1
Dumitru Cantea <canteadumitru@gmail.com>
2015-05-21 10:56:39 +0300

    scripts

---
+++ start.sh
@@ -0,0 +1,1 @@
+forever start ./lib/app.js -c "node"
---
+++ update.sh
@@ -0,0 +1,9 @@
+
+#update repository
+git pull
+#remove modules folder
+#rm -f -r node_modules/
+#npm install new modules
+npm install
+#restart all processes
+forever restartall
